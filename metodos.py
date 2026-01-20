ab1d07bb1b7f7631255d31d6c1cf8fc9f8b0c034
Bruno David Martins <dbrunobruno369@gmail.com>
2026-01-20 00:02:04 -0300

    Metodos para manipulação de lista

---
+++ metodos.py
@@ -0,0 +1,35 @@
+
+lista =[1,2,3]
+
+
+"""
+append()
+    Adiciona um elemento ao final da lista
+"""
+lista.append(4)
+print(lista)
+"""
+insert()
+    Adiciona um elemento em uma posição especifica
+    lista.insert(posição, valor)
+"""
+lista.insert(1,5)
+print(lista)
+"""
+remove()
+    Remove o primeiro elemento encontrado com o valor especificado
+"""
+lista.remove(2)
+print(lista)
+"""
+sort()
+    Ordena os elementos da lista em ordem crescente.
+"""
+lista.sort()
+print(lista)
+"""
+reverse()
+    Reverte a ordem dos elementos da lista
+"""
+lista.reverse()
+print(lista)
