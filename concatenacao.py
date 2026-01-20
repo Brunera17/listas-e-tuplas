b177ec2d301a51dc922063e5f3c45d2970a27804
Bruno David Martins <dbrunobruno369@gmail.com>
2026-01-20 00:07:49 -0300

    concatenação e iteracao

---
+++ concatenacao.py
@@ -0,0 +1,5 @@
+tupla1 = (1,2)
+tupla2 = (3,4)
+
+nova_tupla = tupla1+tupla2
+print(nova_tupla)
---
+++ iteracao.py
@@ -0,0 +1,8 @@
+
+lista = [1,2,3,4]
+for item in lista:
+    print(item)
+
+tupla = (1,2,3,4)
+for item in tupla:
+    print(item)
