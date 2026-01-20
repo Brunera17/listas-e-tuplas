ca3505389833db74f4a21a544864109e8c25e7d9
Bruno David Martins <dbrunobruno369@gmail.com>
2026-01-20 00:15:23 -0300

    Desempacotação

---
+++ desempacotamento.py
@@ -0,0 +1,12 @@
+"""
+    O desempacotamento é uma técnica que permite atribuir os valores de uma tupla ou lista a variáveis de forma mais prática.
+    Isso pe útil quando queremos separar os elementos de uma tupla ou lista de forma direta.
+"""
+
+tupla = (1,2,3)
+a, b, c = tupla
+print(a, b, c)
+
+lista = [10, 20, 30]
+x, y, z = lista
+print(x, y, z)
--- iteracao.py
+++ iteracao.py
@@ -1,3 +1,6 @@
+"""
+    Tanto tuplas quanto listas podem ser iteradas usando laços de repetição, como o for, para acessar seus elementos um por um.
+"""
 
 lista = [1,2,3,4]
 for item in lista:
