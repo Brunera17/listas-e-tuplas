f590309768930924217afe20916eeebdf6e27cac
Bruno David Martins <dbrunobruno369@gmail.com>
2026-01-21 00:49:32 -0300

    desafio 8

---
+++ desafios/8.py
@@ -0,0 +1,20 @@
+"""
+Paulo está criando uma lista de pedidos para a lanchonete. Ele já tem todos os pedidos, mas percebeu que o último foi inserido por engano e precisa removê-lo.
+
+Diante deste problema, ajude Paulo criando um programa que automatize essa operação, permitindo listar os pedidos e remover o último item automaticamente.
+
+Exemplo de Entrada:
+
+    Pedidos feitos (separados por vírgula): Sanduíche, Suco, Sobremesa
+
+Saída esperada: 
+
+    Pedidos finais: ['Sanduíche', 'Suco']
+"""
+
+pedidos = list(input("Pedidos feitos (separados por vírgula): ").split(", "))
+
+pedidos.pop()
+
+print("Pedidos finais: \n")
+print(pedidos)
